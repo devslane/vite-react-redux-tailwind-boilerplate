03c52f3f95634d1eb70b389ca2b8d261536465b8
Madhav Bhasin <madhav.bhasin@devslane.com>
2023-05-30 14:25:21 +0530

    Update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), tsconfigPaths()],
-    server: {
-        open: true,
-    },
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    open: true,
+  },
+  build: { outDir: 'build' },
 });
